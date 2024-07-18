b7a34a68d4bca7dfa50322f652f215b7903e7412
B20DCCN245 - Đinh Trung Hiếu <118317547+hiudtcmd02@users.noreply.github.com>
2024-07-18 09:39:02 +0700

    Chapter 7 (#3)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+
   resources :products
   get "demo_partials/new"
   get "demo_partials/edit"
   get "static_pages/home"
   get "static_pages/help"
+
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: :show
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

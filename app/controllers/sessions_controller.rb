af628a8780a5551fe4dd39f3e4da03779157a2ac
B20DCCN245 - Đinh Trung Hiếu <118317547+hiudtcmd02@users.noreply.github.com>
2024-07-19 15:19:36 +0700

    Chapter 9 (#5)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,14 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params.dig(:session, :email)&.downcase)
     if user&.authenticate(params.dig(:session, :password))
-      # Log the user in and redirect to the user's show page.
-      reset_session
+      # reset_session
       log_in user
+      if params.dig(:session,
+                    :remember_me) == "1"
+        remember(user)
+      else
+        forget(user)
+      end
       redirect_to user, status: :see_other
     else
       flash.now[:danger] = t "controller.sessions_c.invalid"
